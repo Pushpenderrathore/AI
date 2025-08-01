446358f552787ad6991ab744d3538339c2770551
Pushpenderrathore <pushpendersingh046@gmail.com>
2025-08-01 11:55:29 -0400

    modified

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import requests
 import os
 
 app = Flask(__name__)
+CORS(app) 
 
 OLLAMA_URL = "http://localhost:11434/api/generate"
 
