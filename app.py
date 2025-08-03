e54bd4100c13bfee6f77919526aec6b18a7d4b3a
Pushpenderrathore <pushpendersingh046@gmail.com>
2025-08-03 02:17:14 -0400

    modified

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 CORS(app) 
 
 #OLLAMA_URL = os.environ.get("OLLAMA_URL", "http://localhost:11434/api/generate")
-OLLAMA_URL = "https://division-sake-collectibles-earth.trycloudflare.com"
+OLLAMA_URL = "https://division-sake-collectibles-earth.trycloudflare.com/api/generate"
 
 
 @app.route('/ask', methods=['POST'])
